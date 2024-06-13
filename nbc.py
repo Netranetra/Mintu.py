25d08a3f4415a323dec11a37846dbe486c33ae2b
Netra <153888128+Netranetra@users.noreply.github.com>
2024-06-13 10:45:50 +0545

    Update nbc.py

--- nbc.py
+++ nbc.py
@@ -197,15 +197,8 @@ os.system("xdg-open https://m.facebook.com/groups/822430109526151/")
 
 os.system('clear')
 
-import getpass
-
-attemps = 0
-
-while attemps < 12345677901:
-    username = input(' \033[0;92mEnter Username: ')
-    password = input(' \033[0;93mEnter Password: ')
-    if username == '' and password == '':
-        print(' \033[0;92mYou Have Successfully Logged in.')
+import
+ Successfully Logged in.')
         break
 
 logo =""" 
